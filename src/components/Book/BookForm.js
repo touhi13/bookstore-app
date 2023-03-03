044edae48c7cecc6820f33bedc92256712b59e79
Touhidun Nabi Sarkar <touhi13@gmail.com>
2023-03-03 20:12:20 +0600

    final commit

--- src/components/Book/BookForm.js
+++ src/components/Book/BookForm.js
@@ -14,6 +14,7 @@ export default function BookForm() {
     const [rating, setRating] = useState("");
     const [featured, setFeatured] = useState(false);
 
+    //reset data
     const resetData = () => {
         setName("");
         setAuthor("");
